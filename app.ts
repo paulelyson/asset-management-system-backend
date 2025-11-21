b9ba87e0bd558945b8cd552a14db814644b5f1f6
paulelyson <paulelyson.villaceran@gmail.com>
2025-11-21 13:52:56 +0800

    initial commit on express setup

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log('Hello world.')
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const DATABASE = process.env.DATABASE || 'mongodb://127.0.0.1:27017/usjr_ams_local';
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Success get');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
